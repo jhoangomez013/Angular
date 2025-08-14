0e2572f38a5c7c420d61db84dc8a307d96aec0a7
Jhoan Sebastián Gómez Medina <jsgomez013@gmail.com>
2025-08-13 20:55:08 -0500

    configuracion githubP

--- proyecto/src/app/app.ts
+++ proyecto/src/app/app.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { PracticaComponente }  from './practica-componente/practica-componente';
+import { Component, OnInit } from '@angular/core';
+import { PracticaComponente } from './practica-componente/practica-componente';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, PracticaComponente, CommonModule, FormsModule],
+  imports: [FormsModule, PracticaComponente, CommonModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {
+export class App implements OnInit {
   saludo = "Hola";
   esActivo: boolean = true;
   tieneError: boolean = false;
@@ -20,5 +23,22 @@ export class App {
 
   usuarioActivo: boolean = true;
 
-colorFavorito='';
+  colorFavorito = '';
+
+  usuarios: any[] = [];
+
+  constructor(private http: HttpClient) {
+    const miObservable = of('Hola', 'Mundo', ':');
+    miObservable.subscribe(valor => {
+      console.log(valor);
+    });
+  }
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe(data => {
+        this.usuarios = data;
+        console.log(this.usuarios);
+      });
+  }
 }
