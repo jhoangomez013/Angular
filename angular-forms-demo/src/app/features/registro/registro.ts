0d2374e8c342c7dbd04476a95737fcc833d54435
Jhoan Sebastián Gómez Medina <jsgomez013@gmail.com>
2025-08-14 22:33:45 -0500

    campos del formulario y validaciones implementadas

--- angular-forms-demo/src/app/features/registro/registro.ts
+++ angular-forms-demo/src/app/features/registro/registro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,14 +10,21 @@ import { CommonModule } from '@angular/common';
 })
 export class Registro {
   perfil =  new FormGroup({
-    nombre: new FormControl(''),
-    apellido: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    nombre: new FormControl('',[Validators.required, Validators.minLength(2)]),
+    apellido: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
+  get nombre() { return this.perfil.get('nombre'); }
+get apellido() { return this.perfil.get('apellido'); }
+get email() { return this.perfil.get('email'); }    
+get password() { return this.perfil.get('password'); }  
+
 
 enviar() {
-    console.log(this.perfil.value);
-  }
+  console.log(this.perfil.value);
+  alert('Registro exitoso');
+  this.perfil.reset();
+}
 
 }
